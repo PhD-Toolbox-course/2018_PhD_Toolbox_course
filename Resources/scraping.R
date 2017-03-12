5f86f4aec101ffb35cc405911f772ebab16510f0
Marco Chiapello <mc983.cam@gmail.com>
2017-03-12 19:14:38 +0000

    Update scrapping file

--- Resources/scraping.R
+++ Resources/scraping.R
@@ -1,22 +1,33 @@
+# Library
 library(rvest)
+
+# Download data
 lego_movie <- read_html("https://en.wikipedia.org/wiki/Anscombe's_quartet")
 
+# Extract data
 rating <- lego_movie %>% html_table(header = TRUE)
-
 ascomb <- (rating[[2]][2:dim(rating[[2]])[1],])
+
+# Rename data
 names(ascomb) <- paste0(rep(c("x", "y"),4), rep(1:4, each = 2))
+
+# trasform data into data.frame
 df <- data.frame(sapply(ascomb, as.numeric))
 
+# Summary statistics
 summary(df)
 
+# Mean and variance
 round(apply(df, 2, mean), 2)
 round(apply(df, 2, var), 3)
 
+# Data correlation
 cor(df$x1, df$y1)
 cor(df$x2, df$y2)
 cor(df$x3, df$y3)
 cor(df$x4, df$y4)
 
+# Plot
 par(mfrow = c(2,2))
 plot(df$x1, df$y1, main = "Pair 1", xlim = c(4, 20), ylim = c(4,13), pch = 19)
 abline(lm(df$y1 ~ df$x1), col = "red", lty = 2)
