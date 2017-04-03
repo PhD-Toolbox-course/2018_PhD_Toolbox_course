b858ce11edde5abb6ed63e2f4d2de00a43156e0a
Marco Chiapello <mc983.cam@gmail.com>
2017-04-03 17:16:38 +0100

    Improve code

--- Resources/newProjectCreate.R
+++ Resources/newProjectCreate.R
@@ -1,13 +1,19 @@
 date <- Sys.Date()
-#check if tree program is installed, to prodice final schema printout
+
+# check if tree program is installed, to prodice final schema printout
 Sys.which("tree") != "" 
 
+# Set WD
 setwd("~/")
-#getwd()
-(mainDir <- getwd())
+
+# getwd()
+mainDir <- getwd()
+
+# Name of the folder containing your projects
 subDirParent <- "Projects"
-#Customize the following line with your Project Name
-subDirProjectName <- paste0(date,"_deleteme")
+
+# Customize the following line with your Project Name
+subDirProjectName <- paste0(date,"_Your_Project_Cool_Name")
 
 # Create folder structure
 level <- data.frame(level1 = c("data", "data", "doc", "results", "results", "tmp", "code"),
@@ -15,13 +21,15 @@ level <- data.frame(level1 = c("data", "data", "doc", "results", "results", "tmp
 				   )
 
 # Create folders on system
-for (i in 1:7) {
-	dir.create(file.path(mainDir, subDirParent, subDirProjectName, level[i, 1], level[i, 2]), recursive= TRUE, showWarnings = FALSE)
+for (i in 1:nrow(level)) {
+	dir.create(file.path(mainDir, subDirParent, subDirProjectName, level[i, 1], level[i, 2]), recursive = TRUE, showWarnings = FALSE)
 }
 
 # Setup WD on Project folder
 setwd(file.path(mainDir, subDirParent))
-system(paste("tree",subDirProjectName))
+
+# Check folder structure
+system(paste("tree", subDirProjectName))
 
 # Setup WD on new created folder
 setwd(file.path(mainDir, subDirParent, subDirProjectName))
