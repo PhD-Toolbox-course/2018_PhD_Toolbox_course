82baf35db304cef51ac98b5f0f22cc4111af2f43
Marco Chiapello <chiapello.m@gmail.com>
2018-01-22 14:28:37 +0100

    Restore correct folder

--- src/setup.R
+++
@@ -1,6 +0,0 @@
-set.seed(1L)
-
-suppressPackageStartupMessages(library("BiocStyle"))
-suppressPackageStartupMessages(library("DT"))
-suppressPackageStartupMessages(library("knitr"))
-suppressPackageStartupMessages(library("tidyverse"))
