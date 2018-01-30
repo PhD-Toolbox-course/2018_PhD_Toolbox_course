4ae02641c471a8a2190c1a3463be8bd8673ad26b
Marco Chiapello <chiapello.m@gmail.com>
2018-01-30 14:58:44 +0100

    Added script to create the table for the last exercise of day2

---
+++ Resources/Day2/createTable.R
@@ -0,0 +1,6 @@
+n <- 1:10
+sex <- rep(c('male','female'), 5)
+age <- c(23, 22, 21, 22, 24, 30, 23, 29, 19, 29)
+weight <- c(72, 90, 120, 80, 75, 65, 91, 58, 78, 50)
+height <- c(171, 185, 210, 170, 189, 150, 168, 165, 188, 143)
+df <- data.frame(n = n, sex = sex, age = age, weight = weight, height = height)
